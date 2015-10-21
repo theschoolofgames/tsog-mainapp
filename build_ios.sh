95e98777e3b9cba136a4144617de600da91d68e0
Nick <thuydx55vnu@gmail.com>
2015-10-21 23:06:03 +0700

    update build_ios.sh

--- build_ios.sh
+++ build_ios.sh
@@ -1,1 +1,1 @@
-cocos compile -p ios -j 4 -m release --sign-identity "iPhone Developer: Son Tung Nguyen (2248KN5M9U)" --target "tsog" --compile-script 1
+cocos compile -p ios -j 4 -m release --sign-identity "iPhone Developer: Son Tung Nguyen (2248KN5M9U)" --target "tsog" --compile-script 1 --pod
