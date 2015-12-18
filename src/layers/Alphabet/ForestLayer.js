ac395fdde21bd752fb65e28077699057afb28c63
David <david@hub102.com>
2015-12-18 16:48:51 +0700

    fix sounds

--- src/layers/Alphabet/ForestLayer.js
+++ src/layers/Alphabet/ForestLayer.js
@@ -42,8 +42,8 @@ var ForestLayer = cc.Layer.extend({
         this.createBackground();
         // this.showAllAnimals();
         this.createAnimals();
-        // this.addBackButton();
-        // this.addRefreshButton();
+        this.addBackButton();
+        this.addRefreshButton();
         // this.createStarsLabel();
         this.addHud();
         this.runTutorial();
