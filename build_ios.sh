97433f2c41a185ed3be0a241eaf608d38fde64fa
Nick <thuydx55vnu@gmail.com>
2016-01-22 23:14:03 +0700

    test new recorder logic

--- build_ios.sh
+++ build_ios.sh
@@ -1,1 +1,1 @@
-cocos compile -p ios -j 4 --sign-identity "iPhone Developer: Son Tung Nguyen (2248KN5M9U)" --target "tsog" --compile-script 1 --pod
+cocos compile -p ios -j 4 --sign-identity "iPhone Developer: Son Tung Nguyen (2248KN5M9U)" --target "tsog" --compile-script 1 -m release --pod
