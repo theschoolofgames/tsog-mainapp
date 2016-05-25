903449f366ef1a2a6cf8c944ce998a798efd287a
Nick <thuydx55vnu@gmail.com>
2016-05-25 16:55:44 +0700

    fix bug infinity loop when object count = 9

--- build_android.sh
+++ build_android.sh
@@ -1,1 +1,1 @@
-cocos compile -p android -m release --compile-script 1 --android-studio -j 4 --app-abi armeabi:x86
+cocos compile -p android -m debug --compile-script 1 --android-studio -j 4 --app-abi armeabi:x86
