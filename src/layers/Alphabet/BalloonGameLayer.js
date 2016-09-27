0aa1b873d9f68a726931eff0cb0b2d5fe9493d62
blue7un <level.newbie@gmail.com>
2016-09-28 00:31:52 +0700

    - Add gray balloon animation - Increase speed balloon, repeating all words, numbers, colors before completing game.

--- src/layers/Alphabet/BalloonGameLayer.js
+++ src/layers/Alphabet/BalloonGameLayer.js
@@ -3,16 +3,18 @@ var BalloonGameLayer = TestLayer.extend({
     _balloonsLimit: 15,
     _balloons: [],
     _enableSpawn: false,
-    _waitForSpawn: 2.5,
+    _waitForSpawn: 1.0,
     _balloonScale: 1.0,
     _balloonLifeTime: 6,
     _objectsArray: [],
     _tempArray: [],
-    _objectIdArray: ["color_green", "color_red", "color_blue"],
+    _objectIdArray: ["color_green", "number_1", "word_a"],
     _currentObject: null, 
-    _currentIdLabel: null,
+    // _currentIdLabel: null,
     _hudLayer: null,
-    _goalNumber: 10,
+    hudTypeLabel: null,
+    hudBalloonBg: null,
+    // _currentObject.amount: 10,
     _correctChoose: 0,
     _lbGoal: null,
     spriteSheet:null,
@@ -49,15 +51,16 @@ var BalloonGameLayer = TestLayer.extend({
 
         this._currentObject = this._tempArray.pop();
         this._spawnBalloonPool();
-        this._addGoalList(this._currentObject.id, this._goalNumber);
+        // this._addGoalList(this._currentObject.id, this._currentObject.amount);
         this.addHud();
-        // this._addCurrentIdHud(this._currentObject);
+        this._updateCurrentIdHud(this._currentObject);
+        this._updateGoalLabel(0);
 
         this.schedule(this._spawnBalloons, this._waitForSpawn);
     },
 
     _updateGoalLabel: function(correct) {
-        this._hudLayer.updateProgressLabel("".concat(correct).concat("-").concat(this._goalNumber));
+        this._hudLayer.updateProgressLabel("".concat(correct).concat("-").concat(this._currentObject.amount));
     },
 
     // only accept object with type: colors, numbers and alphabets
@@ -103,37 +106,43 @@ var BalloonGameLayer = TestLayer.extend({
         this._hudLayer = hudLayer;
     },
 
-    _addCurrentIdHud: function(currentObj) {
+    _updateCurrentIdHud: function(currentObj) {
         let text = "";
         
         if (currentObj.type === "number" || currentObj.type === "word")
             text = currentObj.value;
 
-        var circle = new cc.Sprite("#balloon_gray_1.png");
-
-        if (currentObj.id.indexOf("red") !== -1){
-            circle.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_red_1.png"));
-        }
-        else if (currentObj.id.indexOf("green") !== -1){
-            circle.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_green_1.png"));
-        }
-        else if (currentObj.id.indexOf("blue") !== -1){
-            circle.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_blue_1.png"));
+        if (!this.hudBalloonBg){
+            this.hudBalloonBg = new cc.Sprite("#balloon_gray_1.png");
+            this.hudBalloonBg.attr({x: 60, y: cc.winSize.height - 200});
+            this.hudBalloonBg.setScale(0.6);
+            // circle.setColor(cc.color.RED);   
+            this.addChild(this.hudBalloonBg);
         }
 
-        circle.attr({x: 60, y: cc.winSize.height - 200});
-        circle.setScale(0.6);
-        // circle.setColor(cc.color.RED);   
-        this.addChild(circle);
+        if (currentObj.value.indexOf("red") !== -1){
+            this.hudBalloonBg.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_red_1.png"));
+        }
+        else if (currentObj.value.indexOf("green") !== -1){
+            this.hudBalloonBg.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_green_1.png"));
+        }
+        else if (currentObj.value.indexOf("blue") !== -1){
+            this.hudBalloonBg.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_blue_1.png"));
+        }
+        else {
+            this.hudBalloonBg.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_gray_1.png"));   
+        }
 
-        var label = new cc.LabelBMFont(text.toUpperCase() + "", "hud-font.fnt");
-        label.setScale(2.0);
-        label.color = cc.color("#ffd902");
-        label.x = circle.width / 2;
-        label.y = circle.height - 60;
-        circle.addChild(label);
+        if (!this.hudTypeLabel){
+            this.hudTypeLabel = new cc.LabelBMFont(text + "", "hud-font.fnt");
+            this.hudTypeLabel.setScale(2.0);
+            this.hudTypeLabel.color = cc.color("#ffd902");
+            this.hudTypeLabel.x = this.hudBalloonBg.width / 2;
+            this.hudTypeLabel.y = this.hudBalloonBg.height - 60;
+            this.hudBalloonBg.addChild(this.hudTypeLabel);
+        }
 
-        this._currentIdLabel = label;
+        this.hudTypeLabel.setString(text + "");
     },
 
     createWarnLabel: function(text, object, x, y) {
@@ -195,17 +204,17 @@ var BalloonGameLayer = TestLayer.extend({
 
                 obj.touched = true;
 
-                if (obj.name === self._currentObject.id){
+                if (obj.name === self._currentObject.value){
                     jsb.AudioEngine.play2d(res.Succeed_sfx);
 
                     self._correctChoose++;
                     self._updateGoalLabel(self._correctChoose);
                     
-                    var percent = self._correctChoose / self._goalNumber;
+                    var percent = self._correctChoose / self._currentObject.amount;
                     self._hudLayer.setProgressBarPercentage(percent);
 
-                    if (self._correctChoose >= self._goalNumber){
-                        self.completedScene();
+                    if (self._correctChoose >= self._currentObject.amount){
+                        self._checkCompleteScene();
                     }
                 }
                 else {
@@ -213,49 +222,57 @@ var BalloonGameLayer = TestLayer.extend({
 
                 }
 
-                if (obj.type != "color"){
-                    // Reset object for reusing
-                    obj.stopAllActions();
+                var labelBalloon = obj.getChildByTag(100);
 
-                    obj.runAction(cc.sequence(cc.fadeOut(1.0), cc.callFunc(() => {
-                        obj.stopAllActions();
-                        obj.x = -100;
-                        obj.y = -100;
-                        obj.setOpacity(255);
-                        obj.touched = false;
-                        obj.setVisible(false);
-                    }, self)));
+                self.redAnimation = new cc.Animation(self.redAnimFrames, 0.1);
+                self.greenAnimation = new cc.Animation(self.greenAnimFrames, 0.1);
+                self.blueAnimation = new cc.Animation(self.blueAnimFrames, 0.1);
+                self.grayAnimation = new cc.Animation(self.grayAnimFrames, 0.1);
+
+                let currentAnimation = self.grayAnimation;
+
+                if (obj.name == "color_blue"){
+                    currentAnimation = self.blueAnimation;
                 }
-                else {
-                    self.redAnimation = new cc.Animation(self.redAnimFrames, 0.1);
-                    self.greenAnimation = new cc.Animation(self.greenAnimFrames, 0.1);
-                    self.blueAnimation = new cc.Animation(self.blueAnimFrames, 0.1);
+                else if (obj.name == "color_green"){
+                    currentAnimation = self.greenAnimation;
+                }
+                else if (obj.name == "color_red"){
+                    currentAnimation = self.redAnimation;
+                }
 
-                    let currentAnimation = self.redAnimation;
-
-                    if (obj.name == "color_blue"){
-                        currentAnimation = self.blueAnimation;
-                    }
-                    else if (obj.name == "color_green"){
-                        currentAnimation = self.greenAnimation;
-                    }
-
-                    // Reset object for reusing
+                // Reset object for reusing
+                obj.stopAllActions();
+                let popAction = new cc.Animate(currentAnimation).repeat(1);
+                obj.runAction(cc.sequence(popAction, cc.callFunc(() => {
                     obj.stopAllActions();
-                    let popAction = new cc.Animate(currentAnimation).repeat(1);
-                    obj.runAction(cc.sequence(popAction, cc.callFunc(() => {
-                        obj.stopAllActions();
-                        obj.x = -100;
-                        obj.y = -100;
-                        obj.setOpacity(255);
-                        obj.touched = false;
-                        obj.setVisible(false);
+                    obj.x = -100;
+                    obj.y = -100;
+                    obj.setOpacity(255);
+                    obj.touched = false;
+                    obj.setVisible(false);
+                }, self)));
+
+                if (obj.type != "color"){
+                    labelBalloon.runAction(cc.sequence(cc.fadeOut(0.3), cc.callFunc(() => {
+                        labelBalloon.stopAllActions();
                     }, self)));
                 }
             }
         });
 
         return true;
+    },
+
+    _checkCompleteScene: function() {
+        if (this._tempArray.length <= 0)
+            self.completedScene();
+        else {
+            this._currentObject = this._tempArray.pop();
+            this._updateCurrentIdHud(this._currentObject);
+            this._updateGoalLabel(0);
+            this._correctChoose = 0;
+        }
     },
 
     _getRandomInt: function(min, max) {
@@ -272,29 +289,30 @@ var BalloonGameLayer = TestLayer.extend({
             return;
 
         var newObjectArray = this._objectsArray.slice(0);
-        newObjectArray.push(this._currentObject);
+        newObjectArray.push(this._currentObject); // For increase frequency goal object
         newObjectArray.push(this._currentObject);
         newObjectArray.push(this._currentObject);
         var randomBalloon = newObjectArray[this._getRandomInt(0, newObjectArray.length)];
-        balloon.name = randomBalloon.id;
+        balloon.name = randomBalloon.value;
         balloon.type = randomBalloon.type;
         balloon.setVisible(true);
 
         var labelBalloon = balloon.getChildByTag(100);
+        labelBalloon.setOpacity(255);
         if (labelBalloon && randomBalloon.type !== "color"){
-            labelBalloon.setString(randomBalloon.value.toUpperCase());
+            labelBalloon.setString(randomBalloon.value);
             balloon.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_gray_1.png"));
         }
         else {
             labelBalloon.setString("");
 
-            if (randomBalloon.id.indexOf("red") !== -1){
+            if (randomBalloon.value.indexOf("red") !== -1){
                 balloon.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_red_1.png"));
             }
-            else if (randomBalloon.id.indexOf("green") !== -1){
+            else if (randomBalloon.value.indexOf("green") !== -1){
                 balloon.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_green_1.png"));
             }
-            else if (randomBalloon.id.indexOf("blue") !== -1){
+            else if (randomBalloon.value.indexOf("blue") !== -1){
                 balloon.setSpriteFrame(cc.spriteFrameCache.getSpriteFrame("balloon_blue_1.png"));
             }
             else {
@@ -326,18 +344,22 @@ var BalloonGameLayer = TestLayer.extend({
         this.redAnimFrames = [];
         this.greenAnimFrames = [];
         this.blueAnimFrames = [];
+        this.grayAnimFrames = [];
         for (var i = 1; i <= 6; i++) {
             var strRed = "balloon_red_" + i + ".png";
             var strGreen = "balloon_green_" + i + ".png";
             var strBlue = "balloon_blue_" + i + ".png";
+            var strGray = "balloon_gray_" + i + ".png";
             this.redAnimFrames.push(cc.spriteFrameCache.getSpriteFrame(strRed));
             this.greenAnimFrames.push(cc.spriteFrameCache.getSpriteFrame(strGreen));
             this.blueAnimFrames.push(cc.spriteFrameCache.getSpriteFrame(strBlue));
+            this.grayAnimFrames.push(cc.spriteFrameCache.getSpriteFrame(strGray));
         }
 
         this.redAnimation = new cc.Animation(this.redAnimFrames, 0.1);
         this.greenAnimation = new cc.Animation(this.greenAnimFrames, 0.1);
         this.blueAnimation = new cc.Animation(this.blueAnimFrames, 0.1);
+        this.grayAnimation = new cc.Animation(this.grayAnimFrames, 0.1);
 
         this._balloons = [];
 
@@ -354,7 +376,7 @@ var BalloonGameLayer = TestLayer.extend({
             balloonSprite.setCascadeOpacityEnabled(true);
 
             var lbBalloon = new cc.LabelBMFont("", res.CustomFont_fnt);
-            lbBalloon.scale = 1.4;
+            lbBalloon.scale = 1.2;
             lbBalloon.x = balloonSprite.width/2;
             lbBalloon.y = balloonSprite.height - 50;
             lbBalloon.tag = 100;
@@ -369,10 +391,11 @@ var BalloonGameLayer = TestLayer.extend({
         // data = JSON.parse(data);
         // cc.log("_fetchObjectData data: " + data);
         if (data)
-            this._objectsArray = data.map(function(id) {
-                var o = GameObject.getInstance().findById(id);
-                if (o[0])
-                    return o[0];
+            this._objectsArray = data.map(function(object) {
+                return object;
+                // var o = GameObject.getInstance().findById(id);
+                // if (o[0])
+                //     return o[0];
             });
         else
             this._data = [];
@@ -411,8 +434,8 @@ var BalloonGameLayer = TestLayer.extend({
     },
 
     _updateGoalLabel: function(correct) {
-        this._lbGoal.setString(correct + "/" + this._goalNumber);
-        this._hudLayer.updateProgressLabel("".concat(correct).concat("-").concat(this._goalNumber));
+        // this._lbGoal.setString(correct + "/" + this._currentObject.amount);
+        this._hudLayer.updateProgressLabel("".concat(correct).concat("-").concat(this._currentObject.amount));
     },
 });
 
