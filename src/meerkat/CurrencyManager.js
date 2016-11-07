d5a17edc1e8006aafe058bf48a8ada0d21764f16
Nick <thuydx55vnu@gmail.com>
2016-11-07 15:49:28 +0700

    add CurrencyManager

---
+++ src/meerkat/CurrencyManager.js
@@ -0,0 +1,43 @@
+var CurrencyManager = cc.Class.extend({
+
+    KEY_COIN: "CurrencyManager:KEY_COIN",
+    KEY_DIAMOND: "CurrencyManager:KEY_DIAMOND",
+
+    ctor: function () {
+        cc.assert(CurrencyManager._instance == null, "can be instantiated once only");
+    },
+
+    getCoin: function() {
+        return KVDatabase.getInstance().getInt(this.KEY_COIN);
+    },
+
+    getDiamond: function() {
+        return KVDatabase.getInstance().getInt(this.KEY_DIAMOND);
+    },
+
+    incCoin: function(c) {
+        if (c > 0)
+            KVDatabase.getInstance().set(this.KEY_COIN, this.getCoin() + c);
+    },
+
+    incDiamond: function(d) {
+        if (d > 0)
+            KVDatabase.getInstance().set(this.KEY_DIAMOND, this.getDiamond() + c);
+    },
+
+    reset: function() {
+        KVDatabase.getInstance().remove(this.KEY_COIN);
+        KVDatabase.getInstance().remove(this.KEY_DIAMOND);
+    }
+});
+
+CurrencyManager._instance = null;
+
+CurrencyManager.getInstance = function () {
+  return CurrencyManager._instance || CurrencyManager.setupInstance();
+};
+
+CurrencyManager.setupInstance = function () {
+    CurrencyManager._instance = new CurrencyManager();
+    return CurrencyManager._instance;
+}
