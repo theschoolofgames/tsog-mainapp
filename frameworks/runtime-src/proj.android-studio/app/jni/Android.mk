3efbedce4d6d5fd93c7b048ad6bbdac8098e3b85
Nick <thuydx55vnu@gmail.com>
2016-11-10 15:55:49 +0700

    update SDKBox, fix android crash

--- frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
+++ frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
@@ -6,33 +6,26 @@ LOCAL_MODULE := cocos2djs_shared
 
 LOCAL_MODULE_FILENAME := libcocos2djs
 
-FILE_LIST := $(wildcard \
-$(LOCAL_PATH)/../../../Classes/*.cpp)
+FILE_LIST := $(wildcard $(LOCAL_PATH)/../../../Classes/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../Classes/h102/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../Classes/h102/js-bindings/*.cpp)
 
 LOCAL_SRC_FILES := hellojavascript/main.cpp
 LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
-LOCAL_WHOLE_STATIC_LIBRARIES := pocketsphinx \
-PluginIAP \
-sdkbox \
-android_native_app_glue
+LOCAL_WHOLE_STATIC_LIBRARIES := pocketsphinx PluginIAP sdkbox android_native_app_glue 
 
 LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
-$(LOCAL_PATH)/../../../Classes/h102 \
-$(LOCAL_PATH)/../../../Classes/h102/js-bindings \
-$(LOCAL_PATH)/../../../../lwf \
-$(LOCAL_PATH)/../../../../SoundTouch \
-$(LOCAL_PATH)/../../../../SoundStretch
+					$(LOCAL_PATH)/../../../Classes/h102 \
+					$(LOCAL_PATH)/../../../Classes/h102/js-bindings \
+					$(LOCAL_PATH)/../../../../lwf \
+					$(LOCAL_PATH)/../../../../SoundTouch \
+					$(LOCAL_PATH)/../../../../SoundStretch
 
-LOCAL_STATIC_LIBRARIES := cocos2d_js_static
-LOCAL_STATIC_LIBRARIES += cocos2d_lwf_static
-LOCAL_STATIC_LIBRARIES += cocos2d_soundstretch_static
+LOCAL_STATIC_LIBRARIES := cocos2d_js_static cocos2d_lwf_static cocos2d_soundstretch_static
 
-LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=2 \
--DCOCOS2D_JAVASCRIPT
+LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=2 -DCOCOS2D_JAVASCRIPT
 LOCAL_LDLIBS := -llog \
 -landroid \
 -llog
@@ -40,7 +33,6 @@ LOCAL_LDLIBS := -llog \
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-add-path,$(LOCAL_PATH))
-$(call import-add-path, $(LOCAL_PATH))
 
 $(call import-module, scripting/js-bindings/proj.android)
 $(call import-module, ../../lwf)
--- frameworks/runtime-src/proj.ios_mac/sdkbox.framework/Versions/A/Headers/Sdkbox.h
+++ frameworks/runtime-src/proj.ios_mac/sdkbox.framework/Versions/A/Headers/Sdkbox.h
@@ -7,7 +7,7 @@
 #ifndef _CC_SDKBOX_H_
 #define _CC_SDKBOX_H_
 
-#define SDKBOX_VERSION_STR  "sdkbox V2.2.4.14"
+#define SDKBOX_VERSION_STR  "sdkbox V2.3.4.6"
 
 /**
  * Only certain compilers support __attribute__((deprecated)).
@@ -81,7 +81,8 @@ namespace sdkbox {
         Platform_Twitter = 1,
         Platform_Facebook = 2,
         Platform_SMS = 5,
-        Platform_EMail = 6,
+        Platform_Mail = 6,
+        //Platform_EMail = Platform_Mail,
 
         Platform_Select = 3,
         Platform_All = 4
