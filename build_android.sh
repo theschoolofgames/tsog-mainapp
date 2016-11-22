d2a6a8637a692846d6800c030b8c31cb87dd3a78
Nick <thuydx55vnu@gmail.com>
2016-11-22 17:56:58 +0700

    update build script

--- build_android.sh
+++ build_android.sh
@@ -1,1 +1,3 @@
 cocos compile -p android -m debug --compile-script 1 --android-studio -j 4 --app-abi armeabi:x86
+cd frameworks/runtime-src/proj.android-studio && ./gradlew crashlyticsUploadSymbolsDebug
+cd ../../..
--- build_android_store.sh
+++ build_android_store.sh
@@ -1,1 +1,3 @@
 cocos compile -p android -m release --compile-script 1 --android-studio -j 4 --app-abi armeabi:x86
+cd frameworks/runtime-src/proj.android-studio && ./gradlew crashlyticsUploadSymbolsRelease
+cd ../../..
