36ad5307a8066b3e5cdad707450451708b56f839
David <david@hub102.com>
2016-12-13 20:05:18 +0700

    fix Enter the game after tap on Language popup layer

--- src/layers/MainApp/ChooseLanguageLayer.js
+++ src/layers/MainApp/ChooseLanguageLayer.js
@@ -12,7 +12,11 @@ var ChooseLanguageLayer = cc.LayerColor.extend({
         this._addDialog();
         this._addLangLabel();
         this._addLangButtonToDialog();
-
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            onTouchBegan: function(touch, event) {return true;}
+        }, this);
     },
 
     _addDialog: function() {
