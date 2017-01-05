2a179c4c5f68bd3a133e41d829cc6871ffb2ffcc
Nick <thuydx55vnu@gmail.com>
2017-01-05 15:27:00 +0700

    fix AR Warning sign position bug

--- src/layers/AlphaRacing/AREffectLayer.js
+++ src/layers/AlphaRacing/AREffectLayer.js
@@ -1,12 +1,13 @@
 var AREffectLayer = cc.Layer.extend({
 
+    _updateYObjects: [],
+
     ctor: function() {
         this._super();
 
-        // var sign = new cc.Sprite("#warning-sign.png");
-        // sign.x = cc.winSize.width/2;
-        // sign.y = cc.winSize.height/2;
-        // this.addChild(sign);
+        this._updateYObjects = [];
+
+        this.scheduleUpdate();
     },
 
     onEnter: function() {
@@ -26,6 +27,19 @@ var AREffectLayer = cc.Layer.extend({
         this._super();
     },
 
+    update: function(dt) {
+        var cameraPos = cc.Camera.getDefaultCamera().getPosition();
+
+        this._updateYObjects.forEach(obj => {
+            var userData = obj.getUserData();
+            if (userData) {
+                if (userData.yPos) {
+                    obj.y = userData.yPos - cameraPos.y + cc.winSize.height/2;
+                }
+            }
+        })
+    },
+
     showFireballWarning: function(event) {
         var yPos = event.getUserData().y;
         var time = event.getUserData().time;
@@ -33,12 +47,22 @@ var AREffectLayer = cc.Layer.extend({
         var sign = new cc.Sprite("#ar-obstacles/warning-sign-1.png");
         sign.x = cc.winSize.width - 50;
         sign.y = yPos;
+        sign.setUserData({
+            yPos: yPos
+        })
         Utils.runAnimation(sign, "ar-obstacles/warning-sign", 0.05, 7, true, 0.05);
         this.addChild(sign);
 
+        this._updateYObjects.push(sign);
+
+        var self = this;
+
         sign.runAction(cc.sequence(
             cc.delayTime(time),
-            cc.callFunc(obj => obj.removeFromParent())
+            cc.callFunc(obj => {
+                self._updateYObjects.splice(self._updateYObjects.indexOf(obj), 1);
+                obj.removeFromParent()
+            })
         ));
     }
 })
