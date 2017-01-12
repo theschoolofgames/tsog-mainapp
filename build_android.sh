26e30fcf12f41dd1fd76084e4b75f6a9cb70f111
Tony Nguyen <tony@hub102.com>
2017-01-13 02:06:58 +0700

    update build android script

--- build_android.sh
+++ build_android.sh
@@ -1,3 +1,3 @@
 cocos compile -p android -m debug --compile-script 1 --android-studio -j 4 --app-abi armeabi:x86
-cd frameworks/runtime-src/proj.android-studio && ./gradlew crashlyticsUploadSymbolsDebug
+cd frameworks/runtime-src/proj.android-studio
 cd ../../..
