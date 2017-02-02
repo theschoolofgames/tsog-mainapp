342e37f6d71b7d57b3cdc3d37c841d20c88a1540
Stefan Nguyen <stnguyenvn@gmail.com>
2017-02-03 02:05:16 +0700

    add build & view log android scripts

---
+++ build_run_android_arm.sh
@@ -0,0 +1,1 @@
+./build_android_arm.sh && adb install -r /Users/stnguyenvn/works/tsog/tsog-main-app/simulator/android/tsog-debug.apk
---
+++ tail_adb_logcat_filter.sh
@@ -0,0 +1,1 @@
+adb logcat | grep -i -E "cocos2d|CC|JNI|TSOG|tsog|error|firebase"
