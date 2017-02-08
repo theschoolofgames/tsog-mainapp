beca22c193c41d40ace3c5767cef1aca11e7e6ad
tranhoangduong1994 <tranhoangduong1994@gmail.com>
2017-02-08 15:25:09 +0700

    rename remote config keys

--- src/meerkat/FirebaseManager.js
+++ src/meerkat/FirebaseManager.js
@@ -65,17 +65,17 @@ var FirebaseManager = cc.Class.extend({
         var config = JSON.parse(data);
         OBJECT_TOTAL_COMPLETED_COUNT = config["object_total_completed_count"] || OBJECT_TOTAL_COMPLETED_COUNT;
 
-        SET_SMALL_PRICE = config["set_small_price"] || SET_SMALL_PRICE;
-        SET_SMALL_COINS = config["set_small_coins"] || SET_SMALL_COINS;
-        SET_SMALL_DIAMONDS = config["set_small_diamonds"] || SET_SMALL_DIAMONDS;
+        SET_SMALL_PRICE = config["set_1_price"] || SET_SMALL_PRICE;
+        SET_SMALL_COINS = config["set_1_coins"] || SET_SMALL_COINS;
+        SET_SMALL_DIAMONDS = config["set_1_diamonds"] || SET_SMALL_DIAMONDS;
 
-        SET_MEDIUM_PRICE = config["set_medium_price"] || SET_MEDIUM_PRICE;
-        SET_MEDIUM_COINS = config["set_medium_coins"] || SET_MEDIUM_COINS;
-        SET_MEDIUM_DIAMONDS = config["set_medium_diamonds"] || SET_MEDIUM_DIAMONDS;
+        SET_MEDIUM_PRICE = config["set_2_price"] || SET_MEDIUM_PRICE;
+        SET_MEDIUM_COINS = config["set_2_coins"] || SET_MEDIUM_COINS;
+        SET_MEDIUM_DIAMONDS = config["set_2_diamonds"] || SET_MEDIUM_DIAMONDS;
 
-        SET_BIG_PRICE = config["set_big_price"] || SET_BIG_PRICE;
-        SET_BIG_COINS = config["set_big_coins"] || SET_BIG_COINS;
-        SET_BIG_DIAMONDS = config["set_big_diamonds"] || SET_BIG_DIAMONDS;
+        SET_BIG_PRICE = config["set_3_price"] || SET_BIG_PRICE;
+        SET_BIG_COINS = config["set_3_coins"] || SET_BIG_COINS;
+        SET_BIG_DIAMONDS = config["set_3_diamonds"] || SET_BIG_DIAMONDS;
     },
 
     setData: function(path, value) {
