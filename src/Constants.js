78c905d7f762cfe078a3a4720b0a82380bfa9f94
Stefan Nguyen <stnguyenvn@gmail.com>
2017-02-27 23:32:08 +0700

    update dynamic link url and twitter page url

--- src/Constants.js
+++ src/Constants.js
@@ -942,7 +942,7 @@ var GAME_OBJECTS_PROGRESS = "game_objects_progress";
 
 var OBJECT_TOTAL_COMPLETED_COUNT = 3;
 
-var DYNAMIC_LINK = "https://ua23d.app.goo.gl/?link=http://www.theschoolofgames.org/?inverter_id%3D%s&apn=com.theschoolofgames.tsog&isi=1090937711&ibi=com.theschoolofgames.tsog"
+var DYNAMIC_LINK = "https://ua23d.app.goo.gl/?link=http://theschoolofgames.org?inverter_id%3D%s&apn=com.theschoolofgames.tsog&isi=1090937711&ibi=com.theschoolofgames.tsog&st=Free%20Download&sd=Pre-school%20app%20for%20children%20aged%202%20-%206%20to%20learn%20vocabulary%2C%20numbers%20and%20reading.&si=https://ucarecdn.com/a898b43b-1ff2-4c47-93eb-7ce583e031c2/share_post.png&utm_source=share_msg&at=share_msg"
 
 var FACEBOOK_SHARING_TITLE = "";
 
@@ -980,7 +980,7 @@ var TEXT_AT_GROWNUP_5 = "Visit us at theschoolofgames.org";
 var GAME_URL_ANDROID = "https://play.google.com/store/apps/details?id=com.theschoolofgames.tsog&hl=en";
 var GAME_URL_IOS = "http://Coming Soon/";
 var FACEBOOK_FAN_PAGE = "https://www.facebook.com/theschoolofgames/";
-var TWITTER_FAN_PAGE = "https://twitter.com/theschoolofgames";
+var TWITTER_FAN_PAGE = "https://twitter.com/schoolofgames_";
 var WEB_URL = "http://www.theschoolofgames.org";
 var PRIVACY_POLICY_URL = "http://www.theschoolofgames.org/privacy-policy/";
 var EMAIL_ADRESS_GAME = "info@theschoolofgames.org";
