5981c283eeb3211221d4513cd4e80d7af09b00cc
Tony <tony@Tonys-iMac.local>
2017-02-27 12:16:19 +0700

    remove character.plist

--- main.js
+++ main.js
@@ -148,7 +148,6 @@ cc.game.onStart = function(){
         cc.spriteFrameCache.addSpriteFrames(res.AR_Lands_plist);
         cc.spriteFrameCache.addSpriteFrames(res.AR_Obstacles_plist);
         cc.spriteFrameCache.addSpriteFrames(res.AR_Boosters_plist);
-        cc.spriteFrameCache.addSpriteFrames(res.Character_plist);
         cc.spriteFrameCache.addSpriteFrames(res.Homescreen_plist);
         cc.spriteFrameCache.addSpriteFrames(res.AlpharacingBG_plist);
         cc.spriteFrameCache.addSpriteFrames(res.Mission_Page_Plist);
@@ -173,15 +172,15 @@ cc.game.onStart = function(){
             debugLog("authenticate result: " + loggedIn);
             // cc.director.runScene(new WelcomeScene());
             // cc.director.runScene(new HomeScene());
-            cc.director.runScene(new MapScene());
+            // cc.director.runScene(new MapScene());
             // cc.director.runScene(new AlphaRacingScene([]));
             // cc.director.runScene(new FirebaseScene());
             // cc.director.runScene(new RewardScene(200,300));
             // cc.director.runScene(new GrownUpMenuScene());
-            // if (loggedIn)
-            //     cc.director.runScene(new WelcomeScene());
-            // else
-            //     cc.director.runScene(new MissionPageBeforeLoginScene());
+            if (loggedIn)
+                cc.director.runScene(new WelcomeScene());
+            else
+                cc.director.runScene(new MissionPageBeforeLoginScene());
             // cc.director.runScene(new MissionPageAfterLoginScene());
         });
 
