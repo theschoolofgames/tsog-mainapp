dc88739e566e1a18693ab7b161fd68de084da190
Tony <tony@Tonys-iMac.local>
2017-03-09 14:55:25 +0700

    fix pod issues when publish app to store, turn off debug mode

--- build_ios_store.sh
+++ build_ios_store.sh
@@ -1,1 +1,1 @@
-cocos compile -p ios -j 4 --sign-identity "iPhone Distribution: anshul dhawan (34F8D32X5G)" --target "tsog" --compile-script 1 -m release --pod
+cocos compile -p ios -j 4 --sign-identity "iPhone Distribution: Equally PBC (34F8D32X5G)" --target "tsog" --compile-script 1 -m release --pod
