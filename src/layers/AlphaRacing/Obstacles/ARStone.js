a90253c7f21e2bb2e38b74ba80069fafd0262ed2
Tony <tony@Tonys-iMac.local>
2017-04-24 16:22:10 +0700

    Reduce the size of bounding/colision box around the rock in alpha racing to half.

--- src/layers/AlphaRacing/Obstacles/ARStone.js
+++ src/layers/AlphaRacing/Obstacles/ARStone.js
@@ -20,7 +20,15 @@ var ARStone = ARObstacle.extend({
     didEnded: function() {
     },
 
-    // update: function(dt) {
+    update: function(dt) {
+        var bBox = this.getBoundingBox();
+        var reducedBBox = cc.rect(bBox.x + bBox.width/4, bBox.y - bBox.height/4, bBox.width/2, bBox.height/2);
 
-    // },
+        if (!this._player.hasBoostFlag(ARInvisible.getBoostFlag()) && cc.rectIntersectsRect(this._player.getBoundingBox(), reducedBBox)) {
+            // cc.log("reducedBBox -> " + JSON.stringify(reducedBBox));
+            // cc.log("this.getBoundingBox() -> " + JSON.stringify(this.getBoundingBox()));
+            // cc.log("this._player.getBoundingBox() -> " + JSON.stringify(this._player.getBoundingBox()));
+            this.onCollide();
+        }
+    },  
 })
