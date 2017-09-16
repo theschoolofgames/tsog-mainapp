8bddff376ab319465533f18e0eb5270acb874803
Steve Nguyen <steve.nguyen.fr@gmail.com>
2017-09-16 23:41:18 +0700

    Hide percentage

--- frameworks/runtime-src/proj.ios_mac/ios/ViewControllers/DetectObjectARKitViewController.m
+++ frameworks/runtime-src/proj.ios_mac/ios/ViewControllers/DetectObjectARKitViewController.m
@@ -751,26 +751,26 @@
     bubbleNode.eulerAngles = SCNVector3Make(0, M_PI, 0);    // Trick to have correct orientation
     
     // Percentage
-    // Bubble text
-    SCNText *percentageText = [SCNText textWithString:[NSString stringWithFormat:@"(%.2f%%)", (confident*100)] extrusionDepth:textDepth];
-    percentageText.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:0.15];
-    percentageText.alignmentMode = kCAAlignmentCenter;
-    percentageText.firstMaterial.diffuse.contents = [UIColor whiteColor];
-    percentageText.firstMaterial.specular.contents = [UIColor blackColor];
-    percentageText.firstMaterial.doubleSided = YES;
-    //    bubbleText.flatness = 1.0;
-    percentageText.chamferRadius = textDepth;
-    
-    // Bubble node
-    SCNVector3 boundingPercentageBoxMin;
-    SCNVector3 boundingPercentageBoxMax;
-    [percentageText getBoundingBoxMin:&boundingPercentageBoxMin max:&boundingPercentageBoxMax];
-    
-    SCNNode *percentageNode = [SCNNode nodeWithGeometry:percentageText];
-    // Centre node - to centre-bottom point
-    percentageNode.pivot = SCNMatrix4MakeTranslation((boundingPercentageBoxMax.x - boundingPercentageBoxMin.x)/2.0, boundingPercentageBoxMin.y + 0.14, textDepth/2.0);
-    percentageNode.scale = SCNVector3Make(0.1, 0.1, 0.1);
-    percentageNode.eulerAngles = SCNVector3Make(0, M_PI, 0);    // Trick to have correct orientation
+//    // Bubble text
+//    SCNText *percentageText = [SCNText textWithString:[NSString stringWithFormat:@"(%.2f%%)", (confident*100)] extrusionDepth:textDepth];
+//    percentageText.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:0.15];
+//    percentageText.alignmentMode = kCAAlignmentCenter;
+//    percentageText.firstMaterial.diffuse.contents = [UIColor whiteColor];
+//    percentageText.firstMaterial.specular.contents = [UIColor blackColor];
+//    percentageText.firstMaterial.doubleSided = YES;
+//    //    bubbleText.flatness = 1.0;
+//    percentageText.chamferRadius = textDepth;
+//
+//    // Bubble node
+//    SCNVector3 boundingPercentageBoxMin;
+//    SCNVector3 boundingPercentageBoxMax;
+//    [percentageText getBoundingBoxMin:&boundingPercentageBoxMin max:&boundingPercentageBoxMax];
+//
+//    SCNNode *percentageNode = [SCNNode nodeWithGeometry:percentageText];
+//    // Centre node - to centre-bottom point
+//    percentageNode.pivot = SCNMatrix4MakeTranslation((boundingPercentageBoxMax.x - boundingPercentageBoxMin.x)/2.0, boundingPercentageBoxMin.y + 0.14, textDepth/2.0);
+//    percentageNode.scale = SCNVector3Make(0.1, 0.1, 0.1);
+//    percentageNode.eulerAngles = SCNVector3Make(0, M_PI, 0);    // Trick to have correct orientation
     
     // Sphere
     // Centre point node
@@ -782,7 +782,7 @@
     SCNNode *bubbleNodeParent = [[SCNNode alloc] init];
     [bubbleNodeParent addChildNode:bubbleNode];
     [bubbleNodeParent addChildNode:sphereNode];
-    [bubbleNodeParent addChildNode:percentageNode];
+//    [bubbleNodeParent addChildNode:percentageNode];
     
     return bubbleNodeParent;
 }
